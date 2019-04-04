c81d9d17a5b34e253d60c23ba27b5fe029cdb9bf
chenyankun <chenyankun@360buyad.local>
2019-04-04 10:58:10 +0800

    sec

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ export default class App extends Component {
     }
 
     navClickFun(navFlag){
-        
         this.setState({
             selectedTab: navFlag
         })
@@ -25,25 +24,28 @@ export default class App extends Component {
         return <Router>
             <div class="mui-content" style={{height: "100%"}}> 
                     {/* <Redirect push to="/goWhere" /> */}
-                    <Route path="/goWhere" component={GoWhereCom} />
-                    <Route path="/withWho" component={WithWhoCom} />
-                    <Route path="/persons" component={PersonsCom} />
-                    <Route path="/my" component={MyCom} />
+                    <Switch>
+                        <Route path="/" exact component={GoWhereCom} />
+                        <Route path="/goWhere" component={GoWhereCom} />
+                        <Route path="/withWho" component={WithWhoCom} />
+                        <Route path="/persons" component={PersonsCom} />
+                        <Route path="/my" component={MyCom} />
+                    </Switch>
             </div>
             <nav class="mui-bar mui-bar-tab">
-                <Link className={["mui-tab-item", this.state.selectedTab=='go'?'mui-active':null].join(' ')}  to="/goWhere" onClick={this.navClickFun.bind(this,'go')}> 
-                    <span class="mui-icon mui-icon-home"></span>
+                <Link className={["mui-tab-item", this.state.selectedTab==='go'?'mui-active':null].join(' ')}  to="/goWhere" onClick={this.navClickFun.bind(this,'go')}> 
+                    <span class="mui-icon icon-qiuchang1"></span>
                     <span class="mui-tab-label">Go</span>
                 </Link>
-                <Link className={["mui-tab-item", this.state.selectedTab=='withWho'?'mui-active':null].join(' ')} to="/withWho" onClick={this.navClickFun.bind(this,'withWho')}>
-                    <span class="mui-icon mui-icon-email"></span>
+                <Link className={["mui-tab-item", this.state.selectedTab==='withWho'?'mui-active':null].join(' ')} to="/withWho" onClick={this.navClickFun.bind(this,'withWho')}>
+                    <span class="mui-icon icon-tubiaozhizuomoban"></span>
                     <span class="mui-tab-label">Who</span>
                 </Link>
-                <Link className={["mui-tab-item", this.state.selectedTab=='persons'?'mui-active':null].join(' ')} to="/persons" onClick={this.navClickFun.bind(this,'persons')}>
+                {/* <Link className={["mui-tab-item", this.state.selectedTab=='persons'?'mui-active':null].join(' ')} to="/persons" onClick={this.navClickFun.bind(this,'persons')}>
                     <span class="mui-icon mui-icon-contact"></span>
                     <span class="mui-tab-label">persons</span>
-                </Link>
-                <Link className={["mui-tab-item", this.state.selectedTab=='my'?'mui-active':null].join(' ')} to="/my" onClick={this.navClickFun.bind(this,'my')}>
+                </Link> */}
+                <Link className={["mui-tab-item", this.state.selectedTab==='my'?'mui-active':null].join(' ')} to="/my" onClick={this.navClickFun.bind(this,'my')}>
                     <span class="mui-icon mui-icon-gear"></span>
                     <span class="mui-tab-label">my</span>
                 </Link>
